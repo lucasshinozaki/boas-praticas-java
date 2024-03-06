457b43e6e109240d3812e1aad072dc0110b62278
Lucas Shinzoaki <https://github.com/lucasshinozaki>
2024-03-06 10:34:30 -0300

    Adicionando mockito e jupiter e criando test unitario de AbrigoService

---
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -0,0 +1,47 @@
+package br.com.alura.service;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.domain.Abrigo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.http.HttpResponse;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AbrigoServiceTest {
+
+    private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
+    private AbrigoService abrigoService = new AbrigoService(client);
+    private HttpResponse<String> response = mock(HttpResponse.class);
+    private Abrigo abrigo = new Abrigo("Teste", "77994563566", "teste@teste.com");
+
+    @Test
+    public void deveVerificarSeDispararRequisicaoGetSeraChamado() throws IOException, InterruptedException {
+        abrigo.setId(0L);
+        String expectedAbrigoCadastrado = "Abrigos cadastrados:";
+        String expectedIdENome = "0 - Teste";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[{"+abrigo.toString()+"}]");
+        when(client.dispararRequisicaoGet(anyString())).thenReturn(response);
+
+        abrigoService.listarAbrigo();
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actualAbrigosCadastrados = lines[0];
+        String actualIdENome = lines[1];
+
+        Assertions.assertEquals(expectedAbrigoCadastrado, actualAbrigosCadastrados);
+        Assertions.assertEquals(expectedIdENome, actualIdENome);
+    }
+
+}
